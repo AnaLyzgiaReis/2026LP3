bb2dc9a31b9f1629a21b471de35dbc0cd07c742e
𝓐𝓷𝓪 <analygiaptc@gmail.com>
2026-02-06 11:38:10 -0300

    Add files via upload

---
+++ LP3_AULA 1/index.php
@@ -0,0 +1,21 @@
+<h1>LP3 AULA 1 - AnaLy</h1>
+
+<?php
+$simb = "ㄟ(≧◇≦)ㄏ";
+
+echo "Hello World! $simb <br><br>";
+
+$x1 = "10";
+$x2 = "5";
+
+$rsoma = $x1 + $x2;
+$rsub = $x1 - $x2;
+$rmult = $x1 * $x2;
+$rdiv = $x1 / $x2;
+
+echo "A soma de $x1 + $x2 = $rsoma <br>";
+echo "A subtração de $x1 - $x2 = $rsub <br>";
+echo "A multiplicação de $x1 x $x2 = $rmult <br>";
+echo "A divisão de $x1 / $x2 = $rdiv <br>";
+
+?>
