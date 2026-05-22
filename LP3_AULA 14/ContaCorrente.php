10ec5fc2697d24a865cf704967fb58cbf70b1a0b
𝓐𝓷𝓪𝓛𝔂  ୨୧ <analygiaptc@gmail.com>
2026-05-22 10:57:05 -0300

    Update ContaCorrente.php

--- LP3_AULA 14/ContaCorrente.php
+++ LP3_AULA 14/ContaCorrente.php
@@ -3,5 +3,11 @@ require_once "Conta.php";
 
 class ContaCorrente extends Conta {
     public float $limiteSaque;
+
+    public function soma(int $a, int $b){
+        $c = $a + $b;
+        if(!empty($x) &&)
+            return $c;
+    }
 }
 ?>
